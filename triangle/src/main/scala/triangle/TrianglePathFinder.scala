b2da36d5de0194ed87ffcb972a2d3babe251dc61
Andrey Demin <ade@cone.ee>
2026-03-08 00:03:28 +0200

    Implement triangle path algorithm with path reconstruction

--- triangle/src/main/scala/triangle/TrianglePathFinder.scala
+++ triangle/src/main/scala/triangle/TrianglePathFinder.scala
@@ -2,27 +2,54 @@ package triangle
 
 import triangle.TrianglePathFinder.PathNode
 
+import scala.annotation.tailrec
+
 object TrianglePathFinder:
   case class PathNode(
-    sum: Int,
-    nextLayerNode: Option[PathNode] // None for bottom layer
+    value: Int,
+    bestSum: Int,
+    next: Option[PathNode] // None for bottom layer
   )
 
 class TrianglePathFinder:
 
   // TODO Add parse error handling
-  private def parseInput(rows: List[String]): Either[String, List[List[Int]]] =
+  def parseInput(rows: List[String]): Either[String, List[List[Int]]] =
     Right(rows.map { s =>
-      s.split("\\s+").map(_.toInt)
-        .toList
+      s.split("\\s+").toList.map(_.toInt)
     })
 
-  private def foldLayers(layers: Seq[List[Int]]): List[List[PathNode]] = {
-    List.empty
+  def foldLayers(layers: List[List[Int]]): Option[PathNode] = {
+    layers.reverse match {
+      case bottomLayer :: upLayers =>
+        val bottomPathNodes = bottomLayer.map(w => PathNode(w, w, Option.empty[PathNode]))
+        upLayers
+          .foldLeft(bottomPathNodes) { case (prevPathLayer, weightsLayer) =>
+            weightsLayer
+              .lazyZip(prevPathLayer)
+              .lazyZip(prevPathLayer.tail)
+              .map { case (w, left, right) =>
+                val betterSide = if (left.bestSum < right.bestSum) left else right
+                PathNode(w, w + betterSide.bestSum, Some(betterSide))
+              }
+          }
+          .headOption
+      case _ => Option.empty[PathNode]
+    }
   }
 
-  private def collectPath(layers: List[List[PathNode]]): List[Int] = {
-    List.empty
+  def collectPath(topNode: Option[PathNode]): List[Int] = {
+    @tailrec
+    def moveDownPath(pn: PathNode, collected: List[PathNode]): List[PathNode] =
+      pn.next match {
+        case Some(next) => moveDownPath(next, pn :: collected)
+        case _ => pn :: collected
+      }
+
+    topNode.fold(List.empty[Int])(
+      moveDownPath(_, List.empty[PathNode])
+        .map(_.value)
+        .reverse)
   }
 
   def findPath(rows: List[String]): Either[String, List[Int]] = {
--- triangle/src/test/scala/triangle/TrianglePathImplSpec.scala
+++ triangle/src/test/scala/triangle/TrianglePathImplSpec.scala
@@ -1,11 +1,28 @@
 package triangle
 
 import org.scalatest.funsuite.AnyFunSuite
+import scala.util.chaining._
+
 
 class TrianglePathImplSpec extends AnyFunSuite:
 
   val trianglePathFinder = new TrianglePathFinder
 
-  test("findPath returns the expected path") {
+  test("findPath returns empty path for empty triangle") {
     assert(trianglePathFinder.findPath(List.empty) == Right(List.empty))
+  }
+
+  test("findPath returns non-empty path for triangle") {
+    val triangle = List(
+      List(7),
+      List(6, 3),
+      List(3, 8, 5),
+      List(11, 2, 10, 9)
+    )
+
+    val path =
+      trianglePathFinder.foldLayers(triangle)
+        .pipe(trianglePathFinder.collectPath)
+
+    assert(path == List(7, 6, 3, 2))
   }
